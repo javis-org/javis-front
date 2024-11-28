9386eda2187ce848a6169b1364e9da3e08c636e3
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-11-29 02:01:52 +0900

    feat: 개발용 배포용 url 수정

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+//개발용
+// const baseUrl = "http://localhost:3000"
+//배포용
+const baseUrl = import.meta.env.DEV 
+  ? "http://localhost:3000"
+  : "https://takaoracle2.duckdns.org";
 export const client = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL:  baseUrl,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
