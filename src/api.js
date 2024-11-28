a02095c555c7eb519dd4db98c2c70420a71aac5a
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-11-29 02:04:43 +0900

    fix:백엔드 배포 url 수정

--- src/api.js
+++ src/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 //개발용
-// const baseUrl = "http://localhost:3000"
 //배포용
 const baseUrl = import.meta.env.DEV 
   ? "http://localhost:3000"
-  : "https://takaoracle2.duckdns.org";
+  : "https://javisbackend.duckdns.org";
 export const client = axios.create({
   baseURL:  baseUrl,
   withCredentials: true,
