89177306c5c8cfc253d1e426dfcaa68a802bf8bb
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-12-08 09:41:13 +0900

    fix:baseurlapi 수정

--- src/api.js
+++ src/api.js
@@ -3,10 +3,10 @@ import axios from "axios";
 //개발용
 //배포용
 const baseUrl = import.meta.env.DEV 
-  ? "http://localhost:3000"
-  : "https://javisbackend.duckdns.org";
+  ? "http://localhost:8080"
+  : "http://javis-LB-205407576.us-east-1.elb.amazonaws.com";
 export const client = axios.create({
-  baseURL:  baseUrl,
+  baseURL: "https://javis.shop",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
