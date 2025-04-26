f4043d88c607443227f93c7711cf8f2db1c1a8c1
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-04-26 20:43:44 +0900

    feat:사이드 메뉴에서 아이템접근 후 뒤로가기시 메뉴 유지

--- src/api.js
+++ src/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-//개발용
-//배포용
-const baseUrl = import.meta.env.DEV 
-  ? "http://localhost:8080"
-  : "http://javis-LB-205407576.us-east-1.elb.amazonaws.com";
 export const client = axios.create({
-  baseURL: "https://javis.shop",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
